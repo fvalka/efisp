36c1078fb78f59dd3266b9612ed17c16fd3eb763
Fabian Valka <fvalka@vektorraum.com>
2019-03-28 00:18:29 +0100

    Prototype for loading G1000 csv file

--- src/app/file-chooser/file-chooser.component.ts
+++ src/app/file-chooser/file-chooser.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var Papa: any;
 
 @Component({
   selector: 'app-file-chooser',
@@ -12,4 +13,15 @@ export class FileChooserComponent implements OnInit {
   ngOnInit() {
   }
 
+  fileLoad(event: Event) {
+    // @ts-ignore
+    Papa.parse((<HTMLInputElement>event.srcElement).files[0], {
+      header: true,
+      dynamicTyping: true,
+      comments: "#",
+      complete: result => console.log(result),
+      transformHeader: input => input.toString().trim(),
+      transform: input => input.toString().trim()
+    });
+  }
 }
